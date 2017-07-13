233a5c6695f4c857ec5ff96a91502f1bbc140754
aldyadk <aldy.andika@gmail.com>
2017-07-13 16:09:59 +0700

    markering

--- src/components/SetPlace.js
+++ src/components/SetPlace.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { View, Text} from 'react-native'
+import axios from 'axios'
 
 import MapView from 'react-native-maps';
 
@@ -9,12 +10,26 @@ class SetPlace extends Component {
     this.state = {
       centerCoordinate:'a',
       region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitude: -6.1751,
+        longitude: 106.8650,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
       },
+      markers:[]
     }
+    this.onRegionChange = this.onRegionChange.bind(this)
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  componentWillMount(){
+    axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.state.region.latitude},${this.state.region.longitude}&radius=500&key=AIzaSyCnuZIr08cpL0ypqUlSX6ZfxZPfHWX_TrA`)
+    .then(response=>{
+      this.setState({'markers':response.data.results})
+      console.log(response);
+    })
   }
 
   render() {
@@ -22,19 +37,44 @@ class SetPlace extends Component {
       <View style={{flex:1}}>
         {this.state.centerCoordinate?(
           <View style={{flex:1}}>
-            <Text style={{flex:1}}>
-              Change type! {this.state.centerCoordinate}
-            </Text>
-            <Text style={{flex:1}}>
-              lat: {this.state.region.latitude}
-            </Text>
+            <View style={{flex:1,flexDirection:'row'}}>
+              <Text style={{flex:1}}>
+                Change type! {this.state.centerCoordinate}
+              </Text>
+              <Text style={{flex:1}}>
+                lat: {this.state.region.latitude}
+              </Text>
+            </View>
+            <View style={{flex:1,flexDirection:'row'}}>
+              <Text style={{flex:1}}>
+                lat: {this.state.region.latitude}
+              </Text>
+              <Text style={{flex:1}}>
+                lng: {this.state.region.longitude}
+              </Text>
+              <Text style={{flex:1}}>
+                latD: {this.state.region.latitudeDelta}
+              </Text>
+              <Text style={{flex:1}}>
+                lngD: {this.state.region.longitudeDelta}
+              </Text>
+            </View>
           </View>
         ):null}
         <MapView
           style={{flex:7}}
           region={this.state.region}
           onRegionChange={this.onRegionChange}
-        />
+        >
+        {this.state.markers.map((marker,index) => (
+          <MapView.Marker
+            key={index}
+            coordinate={{latitude:marker.geometry.location.lat,longitude:marker.geometry.location.lng}}
+            title={marker.name}
+            description={marker.vicinity}
+          />
+        ))}
+        </MapView>
       </View>
     );
   }
