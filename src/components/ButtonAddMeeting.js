17892c6b84388c1955a1932cbc6b4bb986af4e81
aldyadk <aldy.andika@gmail.com>
2017-07-19 20:54:21 +0700

    done tambah styling dan loading

--- src/components/ButtonAddMeeting.js
+++ src/components/ButtonAddMeeting.js
@@ -3,15 +3,12 @@ import { View, StyleSheet } from 'react-native'
 import { Container, Content, Card, CardItem, Text, Body, Button, Icon, Fab } from 'native-base'
 
 class ButtonAddMeeting extends Component {
-  // console.log(this.props)
   render() {
-    // const navigasi = this.props.navigateApp.navigate
-    // console.log('naviga', this.props.navigateApp.navigate);
     const navigate = this.props.navigateApp ? this.props.navigateApp.navigate : (() => {});
     return (
       <Fab
         containerStyle={{ }}
-        style={{ backgroundColor: 'deepskyblue' }}
+        style={{ backgroundColor: '#1aa3ff' }}
         position="bottomRight"
         onPress={() => navigate('CreateMeeting')}>
         <Icon name="add" />
